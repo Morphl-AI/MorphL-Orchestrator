d56a47b431939bf5c4053ed0b68ffdec01adc5c7
Dan Serban <dserban01@yahoo.com>
2018-08-01 22:27:35 +0200

    Dockerization step 4

--- dockerbuilddirs/pythoncontainer/runasroot.sh
+++
@@ -1,9 +0,0 @@
-apt update -qq
-apt -y install \
-  wget curl git vim bzip2 jq mc net-tools less tmux sqlite3 locales sudo ca-certificates
-useradd -m developer
-chmod -R 775 /opt
-chgrp -R developer /opt
-echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
-locale-gen > /dev/null
-
