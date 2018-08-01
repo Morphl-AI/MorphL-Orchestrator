da32a0a77f70fc8fa7a90e0cb6f2aa0ef37074df
Dan Serban <dserban01@yahoo.com>
2018-08-01 17:55:23 +0200

    Update runasnonroot.sh

--- dockerbuilddirs/pythoncontainer/runasnonroot.sh
+++ dockerbuilddirs/pythoncontainer/runasnonroot.sh
@@ -4,4 +4,4 @@ mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
 pip install msgpack
 pip install --upgrade pip
 pip install oauth2client google-api-python-client tensorflow cassandra-driver
-
+echo 'Building container, this may take a while ...'
