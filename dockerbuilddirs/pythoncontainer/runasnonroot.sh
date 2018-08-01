78257d01266ce2eeb5766dd73241cc5bc374af79
Dan Serban <dserban01@yahoo.com>
2018-08-01 22:28:15 +0200

    Delete runasnonroot.sh

--- dockerbuilddirs/pythoncontainer/runasnonroot.sh
+++
@@ -1,7 +0,0 @@
-bash /opt/Anaconda.sh -b -p /opt/anaconda
-rm /opt/Anaconda.sh
-mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
-pip install msgpack
-pip install --upgrade pip
-pip install oauth2client google-api-python-client tensorflow cassandra-driver
-echo 'Building container, this may take a while ...'
