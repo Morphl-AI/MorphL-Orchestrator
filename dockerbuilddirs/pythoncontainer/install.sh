cac517cebc692106c4d01d9ba3d29632ce341dac
Dan Serban <dserban01@yahoo.com>
2018-08-04 18:19:43 +0200

    Update install.sh

--- dockerbuilddirs/pythoncontainer/install.sh
+++ dockerbuilddirs/pythoncontainer/install.sh
@@ -1,7 +1,8 @@
 apt update -qq
 apt -y install locales
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
-locale-gen > /dev/null
+locale-gen en_US en_US.UTF-8 > /dev/null
+update-locale LANG=en_US.UTF-8
 apt -y install \
   wget curl git vim bzip2 jq mc net-tools less tmux sqlite3 sudo ca-certificates
 bash /opt/Anaconda.sh -b -p /opt/anaconda
