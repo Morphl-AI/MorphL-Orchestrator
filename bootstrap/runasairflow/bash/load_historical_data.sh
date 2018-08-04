f04656acb253db62d2af44905450d280b317d9b6
Dan Serban <dserban01@yahoo.com>
2018-08-05 01:30:15 +0200

    Update load_historical_data.sh

--- bootstrap/runasairflow/bash/load_historical_data.sh
+++ bootstrap/runasairflow/bash/load_historical_data.sh
@@ -2,6 +2,8 @@ export TEMPFILE=$(mktemp)
 python /opt/orchestrator/bootstrap/runasairflow/python/load_historical_data.py ${TEMPFILE}
 rc=$?
 if [ ${rc} -eq 0 ]; then
+  echo "Initiating the data load ..."
+  echo
   START_DATE_AS_PY_CODE=$(<${TEMPFILE})
   stop_airflow.sh
   echo "sed 's/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g' /opt/orchestrator/bootstrap/runasairflow/templates/ga_churned_users_dag.py.template" | bash > /home/airflow/airflow/dags/ga_churned_users_dag.py
