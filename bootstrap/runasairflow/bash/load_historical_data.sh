50c36172e9331ed1e60d077e59c18a1ea8eecb9e
Dan Serban <dserban01@yahoo.com>
2018-08-05 16:37:32 +0200

    Update load_historical_data.sh

--- bootstrap/runasairflow/bash/load_historical_data.sh
+++ bootstrap/runasairflow/bash/load_historical_data.sh
@@ -2,10 +2,13 @@ export TEMPFILE=$(mktemp)
 python /opt/orchestrator/bootstrap/runasairflow/python/load_historical_data.py ${TEMPFILE}
 rc=$?
 if [ ${rc} -eq 0 ]; then
+  echo "Emptying the relevant Cassandra tables ..."
+  echo
+  cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /opt/orchestrator/bootstrap/runasairflow/cql/truncate_ga_churned_users_tables.cql
   echo "Initiating the data load ..."
   echo
-  START_DATE_AS_PY_CODE=$(<${TEMPFILE})
   stop_airflow.sh
+  START_DATE_AS_PY_CODE=$(<${TEMPFILE})
   echo "sed 's/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g' /opt/orchestrator/bootstrap/runasairflow/templates/ga_churned_users_dag.py.template" | bash > /home/airflow/airflow/dags/ga_churned_users_dag.py
   start_airflow.sh
   echo "The data load has been initiated."
