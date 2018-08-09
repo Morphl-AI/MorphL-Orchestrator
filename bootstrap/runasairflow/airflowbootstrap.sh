f45228a3a58ee6a5c7813c8d3fff676d86e55afd
Dan Serban <dserban01@yahoo.com>
2018-08-09 23:37:21 +0200

    Update airflowbootstrap.sh

--- bootstrap/runasairflow/airflowbootstrap.sh
+++ bootstrap/runasairflow/airflowbootstrap.sh
@@ -4,6 +4,7 @@ mkdir /home/airflow/.kube
 cat /etc/kubernetes/admin.conf > /home/airflow/.kube/config
 
 SP_CASS_CONN_VERSION=2.3.1
+JSR166E_VERSION=1.1.0
 
 echo 'Setting up Anaconda ...'
 ANACONDA_SH_URL=$(lynx -dump https://repo.continuum.io/archive/ | grep -o http.*Anaconda3.*Linux.x86_64.sh$ | head -1)
@@ -50,6 +51,7 @@ cd /opt/spark/conf
 sed 's/INFO/FATAL/;s/WARN/FATAL/;s/ERROR/FATAL/' log4j.properties.template > log4j.properties
 
 wget -qO /opt/spark/jars/spark-cassandra-connector.jar https://repo1.maven.org/maven2/com/datastax/spark/spark-cassandra-connector_2.11/${SP_CASS_CONN_VERSION}/spark-cassandra-connector_2.11-${SP_CASS_CONN_VERSION}.jar
+wget -qO /opt/spark/jars/jsr166e.jar https://repo1.maven.org/maven2/com/twitter/jsr166e/${JSR166E_VERSION}/jsr166e-${JSR166E_VERSION}.jar
 
 echo 'Setting up Hadoop ...'
 HADOOP_TGZ_URL=$(lynx -dump ${MIRROR}hadoop/common/stable/ | grep -o http.*gz$ | grep -v src | head -1)
