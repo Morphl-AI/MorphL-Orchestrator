fe6a2e162ac205337e90698762b0718e1a1eb36d
Dan Serban <dserban01@yahoo.com>
2018-08-11 20:43:09 +0200

    Update install.sh

--- dockerbuilddirs/pythoncontainer/install.sh
+++ dockerbuilddirs/pythoncontainer/install.sh
@@ -1,11 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -qq
-apt -y install locales apt-utils
+apt -y install locales apt-utils &>/dev/null
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen > /dev/null
 update-locale LANG=en_US.UTF-8
-apt -y install \
-  wget curl git vim bzip2 jq mc lynx net-tools less tmux sqlite3 sudo ca-certificates > /dev/null
+apt -y install wget curl git vim bzip2 jq mc lynx net-tools less tmux sqlite3 sudo ca-certificates &>/dev/null
 bash /opt/Anaconda.sh -b -p /opt/anaconda
 rm /opt/Anaconda.sh
 mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
