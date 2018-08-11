2470c3b03eadfcad7dba47aedd5b0b9f2aec215d
Dan Serban <dserban01@yahoo.com>
2018-08-11 16:58:24 +0200

    Update install.sh

--- dockerbuilddirs/pythoncontainer/install.sh
+++ dockerbuilddirs/pythoncontainer/install.sh
@@ -5,7 +5,7 @@ echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen > /dev/null
 update-locale LANG=en_US.UTF-8
 apt -y install \
-  wget curl git vim bzip2 jq mc net-tools less tmux sqlite3 sudo ca-certificates
+  wget curl git vim bzip2 jq mc lynx net-tools less tmux sqlite3 sudo ca-certificates
 bash /opt/Anaconda.sh -b -p /opt/anaconda
 rm /opt/Anaconda.sh
 mv /opt/anaconda/bin/sqlite3 /opt/anaconda/bin/sqlite3.orig
