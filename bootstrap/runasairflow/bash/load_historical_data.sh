d0ecf0290f322b91c73de7d6087aea4993056236
Dan Serban <dserban01@yahoo.com>
2018-08-13 22:13:32 +0200

    Update load_historical_data.sh

--- bootstrap/runasairflow/bash/load_historical_data.sh
+++ bootstrap/runasairflow/bash/load_historical_data.sh
@@ -9,8 +9,8 @@ if [ ${rc} -eq 0 ]; then
   echo
   stop_airflow.sh
   airflow resetdb -y &>/dev/null
-  START_DATE_AS_PY_CODE=$(<${TEMPFILE})
-  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g" /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_churned_users_step_1_dag.py.template > /home/airflow/airflow/dags/ga_churned_users_step_1_dag.py
+  STEP_1_START_DATE_AS_PY_CODE=$(<${TEMPFILE})
+  sed "s/STEP_1_START_DATE_AS_PY_CODE/${STEP_1_START_DATE_AS_PY_CODE}/g" /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_churned_users_step_1_dag.py.template > /home/airflow/airflow/dags/ga_churned_users_step_1_dag.py
   start_airflow.sh
   echo "The data load has been initiated."
   echo
