420ef21cb44b8431a8814fa5f76311089bba95b1
Dan Serban <dserban01@yahoo.com>
2018-08-13 23:28:40 +0200

    Update load_historical_data.py

--- bootstrap/runasairflow/python/load_historical_data.py
+++ bootstrap/runasairflow/python/load_historical_data.py
@@ -3,15 +3,15 @@ from sys import argv, exit
 
 def get_record(i, num_days_ago, ref_dt):
     dt = ref_dt - datetime.timedelta(days=num_days_ago)
+    one_day_prior = ref_dt - datetime.timedelta(days=num_days_ago+1)
     return (i, {'days_worth_of_data_to_load': str(num_days_ago),
                 'asYYYY-MM-DD': dt.strftime('%Y-%m-%d'),
-                'as_py_code': dt.__repr__()})
+                'as_py_code': one_day_prior.__repr__()})
 
 OPTIONS = [5, 10, 30, 60, 120, 180, 270, 365]
 opt_len = len(OPTIONS)
 valid_inputs = set([str(i+1) for i in range(opt_len)])
 n = datetime.datetime.now()
-yesterday = n - datetime.timedelta(days=1)
 tomorrow = n + datetime.timedelta(days=1)
 lookup_dict = \
     dict([get_record(i + 1, num_days_ago, n) for (i, num_days_ago) in enumerate(OPTIONS)])
