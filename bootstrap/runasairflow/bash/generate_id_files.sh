ca9f14190e147a6c0a652c49326011131dd275cf
Dan Serban <dserban01@yahoo.com>
2018-08-29 15:49:49 +0200

    Update generate_id_files.sh

--- bootstrap/runasairflow/bash/generate_id_files.sh
+++ bootstrap/runasairflow/bash/generate_id_files.sh
@@ -1,6 +1,6 @@
 TODAY_AS_STR=$(date +"%Y-%m-%d")
 UNIQUE_HASH=$(openssl rand -hex 64 | cut -c1-20)
-IS_MODEL_VALID=false
+IS_MODEL_VALID=False
 echo ${TODAY_AS_STR} > /tmp/today_as_str.txt
 echo ${UNIQUE_HASH} > /tmp/unique_hash.txt
 sed "s/TODAY_AS_STR/${TODAY_AS_STR}/;s/UNIQUE_HASH/${UNIQUE_HASH}/;s/IS_MODEL_VALID/${IS_MODEL_VALID}/" /opt/orchestrator/bootstrap/runasairflow/cql/insert_into_valid_models.cql.template > /tmp/insert_into_valid_models.cql
