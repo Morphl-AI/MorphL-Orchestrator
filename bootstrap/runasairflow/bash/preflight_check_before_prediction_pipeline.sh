f08c0cff09e60d93f5c4c7229a2c61536b967d4f
Dan Serban <dserban01@yahoo.com>
2018-08-29 22:22:50 +0200

    Update preflight_check_before_prediction_pipeline.sh

--- bootstrap/runasairflow/bash/preflight_check_before_prediction_pipeline.sh
+++ bootstrap/runasairflow/bash/preflight_check_before_prediction_pipeline.sh
@@ -1,6 +1,5 @@
 cql_stmt='SELECT is_model_valid FROM morphl.ga_churned_users_valid_models WHERE always_zero = 0 AND is_model_valid = True LIMIT 1 ALLOW FILTERING;'
-echo ${cql_stmt} | \
-  cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} | \
+  cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -e "${cql_stmt}" | \
   grep True && \
     airflow trigger_dag ga_chu_prediction_pipeline
 exit 0
