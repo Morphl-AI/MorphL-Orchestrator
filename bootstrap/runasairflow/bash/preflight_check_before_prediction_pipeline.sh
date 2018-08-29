3439a2b9028d53408b769e3e167af05b6235ff10
Dan Serban <dserban01@yahoo.com>
2018-08-30 01:44:27 +0200

    Refactor Cassandra table names

--- bootstrap/runasairflow/bash/preflight_check_before_prediction_pipeline.sh
+++ bootstrap/runasairflow/bash/preflight_check_before_prediction_pipeline.sh
@@ -1,4 +1,4 @@
-cql_stmt='SELECT is_model_valid FROM morphl.ga_churned_users_valid_models WHERE always_zero = 0 AND is_model_valid = True LIMIT 1 ALLOW FILTERING;'
+cql_stmt='SELECT is_model_valid FROM morphl.ga_chu_valid_models WHERE always_zero = 0 AND is_model_valid = True LIMIT 1 ALLOW FILTERING;'
   cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -e "${cql_stmt}" | \
   grep True && \
     airflow trigger_dag ga_chu_prediction_pipeline
