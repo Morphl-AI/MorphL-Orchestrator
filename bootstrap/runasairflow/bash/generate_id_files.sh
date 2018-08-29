1383f806ec19262539dbf9eb6d76c66e37057f8b
Dan Serban <dserban01@yahoo.com>
2018-08-29 16:59:09 +0200

    Update generate_id_files.sh

--- bootstrap/runasairflow/bash/generate_id_files.sh
+++ bootstrap/runasairflow/bash/generate_id_files.sh
@@ -1,7 +1,7 @@
 TODAY_AS_STR=$(date +"%Y-%m-%d")
 UNIQUE_HASH=$(openssl rand -hex 64 | cut -c1-20)
 IS_MODEL_VALID=False
-echo ${TODAY_AS_STR} > /tmp/today_as_str.txt
-echo ${UNIQUE_HASH} > /tmp/unique_hash.txt
-sed "s/TODAY_AS_STR/${TODAY_AS_STR}/;s/UNIQUE_HASH/${UNIQUE_HASH}/;s/IS_MODEL_VALID/${IS_MODEL_VALID}/" /opt/orchestrator/bootstrap/runasairflow/cql/insert_into_valid_models.cql.template > /tmp/insert_into_valid_models.cql
-cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /tmp/insert_into_valid_models.cql
+echo ${TODAY_AS_STR} > /tmp/ga_chu_training_pipeline_today_as_str.txt
+echo ${UNIQUE_HASH} > /tmp/ga_chu_training_pipeline_unique_hash.txt
+sed "s/TODAY_AS_STR/${TODAY_AS_STR}/;s/UNIQUE_HASH/${UNIQUE_HASH}/;s/IS_MODEL_VALID/${IS_MODEL_VALID}/" /opt/orchestrator/bootstrap/runasairflow/cql/insert_into_valid_models.cql.template > /tmp/ga_chu_training_pipeline_insert_into_valid_models.cql
+cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /tmp/ga_chu_training_pipeline_insert_into_valid_models.cql
