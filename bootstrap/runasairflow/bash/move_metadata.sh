b717dd81b94c2b72f945a3450b691521085d0e17
Dan Serban <dserban01@yahoo.com>
2018-08-30 02:00:17 +0200

    Refactor HDFS dir name

--- bootstrap/runasairflow/bash/move_metadata.sh
+++ bootstrap/runasairflow/bash/move_metadata.sh
@@ -1,4 +1,4 @@
-HDFS_DIR=/preproc_${DAY_AS_STR}_${UNIQUE_HASH}
+HDFS_DIR=/${DAY_AS_STR}_{UNIQUE_HASH}_preproc_training
 
 hdfs dfs -mv ${HDFS_DIR}/_metadata ${HDFS_DIR}/_md
 hdfs dfs -mkdir ${HDFS_DIR}/_metadata
