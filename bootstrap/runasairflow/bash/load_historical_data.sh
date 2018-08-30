db7bce0df868a7856d5d4e0bd3bb1ecff7b61ea0
Dan Serban <dserban01@yahoo.com>
2018-08-30 16:00:28 +0200

    Adjustments to prediction pipeline

--- bootstrap/runasairflow/bash/load_historical_data.sh
+++ bootstrap/runasairflow/bash/load_historical_data.sh
@@ -19,7 +19,7 @@ if [ ${rc} -eq 0 ]; then
   sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g" /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_chu_ingestion_pipeline.py.template > /home/airflow/airflow/dags/ga_chu_ingestion_pipeline.py
   START_DATE_AS_PY_CODE=$(<${TEMPFILE_B})
   sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g" /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_chu_training_pipeline.py.template > /home/airflow/airflow/dags/ga_chu_training_pipeline.py
-  cat /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_chu_prediction_pipeline.py > /home/airflow/airflow/dags/ga_chu_prediction_pipeline.py
+  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g" /opt/orchestrator/bootstrap/runasairflow/python/dags/ga_chu_prediction_pipeline.py.template > /home/airflow/airflow/dags/ga_chu_prediction_pipeline.py
   start_airflow.sh
   echo 'The data load has been initiated.'
   echo
