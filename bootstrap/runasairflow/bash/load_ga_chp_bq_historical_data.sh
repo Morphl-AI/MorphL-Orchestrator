9c5033c7210bc007fc71c42b62a2afc952bf7767
Alexandra Anghel <alexandra@appticles.com>
2018-12-07 14:38:51 +0200

    Refactor historical data loading to use training and prediction interval

--- bootstrap/runasairflow/bash/load_ga_chp_bq_historical_data.sh
+++ bootstrap/runasairflow/bash/load_ga_chp_bq_historical_data.sh
@@ -1,21 +1,21 @@
-# TEMPFILE_A is the training window delay (difference between end date and today)
+# TEMPFILE_A is the duration of the training interval in days
 export TEMPFILE_A=$(mktemp)
-# TEMPFILE_B is the duration of the training interval in days
+# TEMPFILE_B is the duration of the predictions interval in days
 export TEMPFILE_B=$(mktemp)
 # TEMPFILE_C is the Python start date (today)
 export TEMPFILE_C=$(mktemp)
 
-python /opt/ga_chp_bq/training/pipeline_setup/ga_chp_bq_load_historical_data.py ${TEMPFILE_A} ${TEMPFILE_B} ${TEMPFILE_C}
+python /opt/ga_chp_bq/bq_extractor/ga_chp_bq_load_historical_data.py ${TEMPFILE_A} ${TEMPFILE_B} ${TEMPFILE_C}
 rc=$?
 if [ ${rc} -eq 0 ]; then
   echo 'Emptying the relevant Cassandra tables ...'
   echo
-  cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /opt/ga_chp_bq/training/pipeline_setup/ga_chp_bq_truncate_tables_before_training_pipeline.cql
+  cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /opt/ga_chp_bq/bq_extractor/ga_chp_bq_truncate_tables_before_loading_historical_data.cql
 
   # Write configuration parameters in corresponding Cassandra table
-  DAYS_TRAINING_DELAY=$(<${TEMPFILE_A})
-  DAYS_WORTH_OF_DATA_TO_LOAD=$(<${TEMPFILE_B})
-  sed "s/DAYS_TRAINING_DELAY/${DAYS_TRAINING_DELAY}/g;s/DAYS_WORTH_OF_DATA_TO_LOAD/${DAYS_WORTH_OF_DATA_TO_LOAD}/g" /opt/ga_chp_bq/training/pipeline_setup/insert_into_ga_chp_bq_config_parameters.cql.template > /tmp/insert_into_config_parameters.cql
+  DAYS_TRAINING_INTERVAL=$(<${TEMPFILE_A})
+  DAYS_PREDICTION_INTERVAL=$(<${TEMPFILE_B})
+  sed "s/DAYS_TRAINING_INTERVAL/${DAYS_TRAINING_INTERVAL}/g;s/DAYS_PREDICTION_INTERVAL/${DAYS_PREDICTION_INTERVAL}/g" /opt/ga_chp_bq/training/pipeline_setup/insert_into_ga_chp_bq_config_parameters.cql.template > /tmp/insert_into_config_parameters.cql
   cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} -f /tmp/insert_into_config_parameters.cql
 
   # Reset Airflow and create dags
@@ -28,12 +28,12 @@ if [ ${rc} -eq 0 ]; then
   
   # Create training dag and trigger pipeline
   START_DATE_AS_PY_CODE=$(<${TEMPFILE_C})
-  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g;s/DAYS_TRAINING_DELAY/${DAYS_TRAINING_DELAY}/g;s/DAYS_WORTH_OF_DATA_TO_LOAD/${DAYS_WORTH_OF_DATA_TO_LOAD}/g" /opt/ga_chp_bq/training/pipeline_setup/ga_chp_bq_training_airflow_dag.py.template > /home/airflow/airflow/dags/ga_chp_bq_training_pipeline.py
+  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g;s/DAYS_TRAINING_INTERVAL/${DAYS_TRAINING_INTERVAL}/g;s/DAYS_PREDICTION_INTERVAL/${DAYS_PREDICTION_INTERVAL}/g" /opt/ga_chp_bq/training/pipeline_setup/ga_chp_bq_training_airflow_dag.py.template > /home/airflow/airflow/dags/ga_chp_bq_training_pipeline.py
   airflow trigger_dag ga_chp_bq_training_pipeline
   
   # Create prediction dag
   START_DATE_AS_PY_CODE=$(<${TEMPFILE_C})
-  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g;s/DAYS_TRAINING_DELAY/${DAYS_TRAINING_DELAY}/g" /opt/ga_chp_bq/prediction/pipeline_setup/ga_chp_bq_prediction_airflow_dag.py.template > /home/airflow/airflow/dags/ga_chp_bq_prediction_pipeline.py
+  sed "s/START_DATE_AS_PY_CODE/${START_DATE_AS_PY_CODE}/g;s/DAYS_PREDICTION_INTERVAL/${DAYS_PREDICTION_INTERVAL}/g" /opt/ga_chp_bq/prediction/pipeline_setup/ga_chp_bq_prediction_airflow_dag.py.template > /home/airflow/airflow/dags/ga_chp_bq_prediction_pipeline.py
   
   start_airflow.sh
   echo 'The data load has been initiated.'
